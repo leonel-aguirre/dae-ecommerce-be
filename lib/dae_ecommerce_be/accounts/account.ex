198b74e7b764791843d88b86d2807b92703536db
Leonel Aguirre <leonel.aguirre182@gmail.com>
2024-04-29 00:48:52 -0600

    DEBE-1 - Account JSON resource files generated.

---
+++ lib/dae_ecommerce_be/accounts/account.ex
@@ -0,0 +1,20 @@
+defmodule DaeEcommerceBe.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :password, :string
+    field :email, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+  end
+end
---
+++ test/dae_ecommerce_be_web/controllers/account_controller_test.exs
@@ -0,0 +1,88 @@
+defmodule DaeEcommerceBeWeb.AccountControllerTest do
+  use DaeEcommerceBeWeb.ConnCase
+
+  import DaeEcommerceBe.AccountsFixtures
+
+  alias DaeEcommerceBe.Accounts.Account
+
+  @create_attrs %{
+    password: "some password",
+    email: "some email"
+  }
+  @update_attrs %{
+    password: "some updated password",
+    email: "some updated email"
+  }
+  @invalid_attrs %{password: nil, email: nil}
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "index" do
+    test "lists all accounts", %{conn: conn} do
+      conn = get(conn, ~p"/api/accounts")
+      assert json_response(conn, 200)["data"] == []
+    end
+  end
+
+  describe "create account" do
+    test "renders account when data is valid", %{conn: conn} do
+      conn = post(conn, ~p"/api/accounts", account: @create_attrs)
+      assert %{"id" => id} = json_response(conn, 201)["data"]
+
+      conn = get(conn, ~p"/api/accounts/#{id}")
+
+      assert %{
+               "id" => ^id,
+               "email" => "some email",
+               "password" => "some password"
+             } = json_response(conn, 200)["data"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, ~p"/api/accounts", account: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "update account" do
+    setup [:create_account]
+
+    test "renders account when data is valid", %{conn: conn, account: %Account{id: id} = account} do
+      conn = put(conn, ~p"/api/accounts/#{account}", account: @update_attrs)
+      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+
+      conn = get(conn, ~p"/api/accounts/#{id}")
+
+      assert %{
+               "id" => ^id,
+               "email" => "some updated email",
+               "password" => "some updated password"
+             } = json_response(conn, 200)["data"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, account: account} do
+      conn = put(conn, ~p"/api/accounts/#{account}", account: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "delete account" do
+    setup [:create_account]
+
+    test "deletes chosen account", %{conn: conn, account: account} do
+      conn = delete(conn, ~p"/api/accounts/#{account}")
+      assert response(conn, 204)
+
+      assert_error_sent 404, fn ->
+        get(conn, ~p"/api/accounts/#{account}")
+      end
+    end
+  end
+
+  defp create_account(_) do
+    account = account_fixture()
+    %{account: account}
+  end
+end
