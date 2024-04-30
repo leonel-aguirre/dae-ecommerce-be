0b488c1e515da9a1074526d1fa68db7f03a55f2a
Leonel Aguirre <leonel.aguirre182@gmail.com>
2024-04-29 23:39:12 -0600

    DEBE-1 - Pipeline for endpoints that requires authentication added.

--- lib/dae_ecommerce_be_web/router.ex
+++ lib/dae_ecommerce_be_web/router.ex
@@ -18,11 +18,22 @@ defmodule DaeEcommerceBeWeb.Router do
     plug :accepts, ["json"]
   end
 
+  # Ensures authentication before triggering the service.
+  pipeline :auth do
+    plug DaeEcommerceBeWeb.Auth.Pipeline
+  end
+
   scope "/api", DaeEcommerceBeWeb do
     pipe_through :api
 
     get "/", DefaultController, :index
     post "/accounts/create", AccountController, :create
     post "/accounts/sign_in", AccountController, :sign_in
+  end
+
+  # Authentication required.
+  scope "/api", DaeEcommerceBeWeb do
+    pipe_through [:api, :auth]
+    get "/accounts/by_id/:id", AccountController, :show
   end
 end
